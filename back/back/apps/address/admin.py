19cd5a2215b055d00b8d722922a76bf2751a1564
Vixx_ <vittorio.adesso@gmail.com>
2022-12-01 22:30:43 -0400

    feat: admin tables address

--- back/back/apps/address/admin.py
+++ back/back/apps/address/admin.py
@@ -12,21 +12,21 @@ class ContinentListAdmin(admin.ModelAdmin):
 @admin.register(models.Country)
 class CountryListAdmin(admin.ModelAdmin):
     search_fields = ("iso_3166_1_a2", "name", "printable_name")
-    list_display = ("id", "name", "printable_name", "continent__name")
-    list_filter = ("continent__name",)
+    list_display = ("name", "printable_name", "continent")
+    list_filter = ("continent",)
 
 
 @admin.register(models.State)
 class StateListAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
-    list_display = ("name", "country__name")
+    search_fields = ("name", "country__name")
+    list_display = ("id", "name", "country")
     list_filter = ("country",)
 
 
 @admin.register(models.City)
 class CityListAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
-    list_display = ("name", "state__name")
+    search_fields = ("name", "state__name", "state__country__name")
+    list_display = ("id", "name", "state")
     list_filter = ("state",)
 
 
