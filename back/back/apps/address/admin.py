83b0a7c34224ce824e7fdca62909ebd8d488089f
Vixx_ <vittorio.adesso@gmail.com>
2022-12-01 22:22:37 -0400

    feat: admin tables address

--- back/back/apps/address/admin.py
+++ back/back/apps/address/admin.py
@@ -2,8 +2,32 @@ from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from . import models
 
-admin.site.register(models.Continent)
-admin.site.register(models.Country)
-admin.site.register(models.State)
-admin.site.register(models.City)
+
+@admin.register(models.Continent)
+class ContinentListAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("id", "name")
+
+
+@admin.register(models.Country)
+class CountryListAdmin(admin.ModelAdmin):
+    search_fields = ("iso_3166_1_a2", "name", "printable_name")
+    list_display = ("id", "name", "printable_name", "continent__name")
+    list_filter = ("continent__name",)
+
+
+@admin.register(models.State)
+class StateListAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "country__name")
+    list_filter = ("country",)
+
+
+@admin.register(models.City)
+class CityListAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "state__name")
+    list_filter = ("state",)
+
+
 admin.site.register(models.Address)
