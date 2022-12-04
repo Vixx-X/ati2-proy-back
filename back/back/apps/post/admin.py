b89afe6652edc845371b17305bd88cbbbb738d50
Vixx_ <vittorio.adesso@gmail.com>
2022-12-04 18:00:38 -0400

    feat: posts

--- back/back/apps/post/admin.py
+++ back/back/apps/post/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from .models import ContactSeller, DayOption
 
+from back.apps.vehicle.models import VehiclePost
+from back.apps.job.models import JobPost
+from back.apps.service.models import ServicePost
+
 admin.site.register(DayOption)
 admin.site.register(ContactSeller)
+
+admin.site.register(VehiclePost)
+admin.site.register(JobPost)
+admin.site.register(ServicePost)
