04cc14ff674e3d5d933d9f0ca83d27092511535e
Vixx_ <vittorio.adesso@gmail.com>
2022-12-04 18:25:48 -0400

    feat: vehicle

--- back/back/apps/post/serializers.py
+++ back/back/apps/post/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
-from .models import ContactSeller, DayOption
+from .models import ContactSeller, DayOption, Contact
 
 
 class ContactSellerSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactSeller
+        fields = "__all__"
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
         fields = "__all__"
 
 
--- back/back/apps/vehicle/serializers.py
+++ back/back/apps/vehicle/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
 from back.apps.address.serializers import AddressSerializer
-from back.apps.post.serializers import ContactSellerSerializer
+from back.apps.post.serializers import ContactSerializer
 from back.apps.user.models import User
 
 from .models import Vehicle, VehiclePost
@@ -34,7 +34,7 @@ class VehicleSerializer(serializers.ModelSerializer):
 
 
 class VehiclePostSerializer(serializers.ModelSerializer):
-    contact = ContactSellerSerializer()
+    contact = ContactSerializer()
     address = AddressSerializer()
     author = serializers.PrimaryKeyRelatedField(
         default=serializers.CurrentUserDefault(),
@@ -54,7 +54,7 @@ class VehiclePostSerializer(serializers.ModelSerializer):
         contact = validated_data.pop("contact")
         address = validated_data.pop("address")
 
-        contact = ContactSellerSerializer().create(contact)
+        contact = ContactSerializer().create(contact)
         validated_data["contact"] = contact
 
         address = AddressSerializer().create(address)
