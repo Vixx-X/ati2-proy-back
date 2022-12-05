f0922c4f7779d1c4c166fffd5b1b782185197469
Vixx_ <vittorio.adesso@gmail.com>
2022-12-05 14:20:19 -0400

    fix: media separated in videos and images

--- back/back/apps/user/permissions.py
+++ back/back/apps/user/permissions.py
@@ -27,9 +27,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # view must have the instance attribute named `user_attr` else `user`.
-        return not request.user.is_authenticated
+        return bool(request.user and request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         # Only create object new objects,
