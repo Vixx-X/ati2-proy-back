da7630ec29ce36a45bc6f85fb30cd3dce0eead80
Vixx_ <vittorio.adesso@gmail.com>
2022-12-07 16:57:21 -0400

    fix: address filter

--- back/back/apps/address/filters.py
+++ back/back/apps/address/filters.py
@@ -6,16 +6,30 @@ from . import models
 class CountryFilter(filters.FilterSet):
     class Meta:
         model = models.Country
-        fields = ["continent"]
+        fields = "__all__"
 
 
 class StateFilter(filters.FilterSet):
+
+    continent = filters.Filter(
+        field_name="country__continent",
+    )
+
     class Meta:
         model = models.State
-        fields = ["country"]
+        fields = "__all__"
 
 
 class CityFilter(filters.FilterSet):
+
+    continent = filters.Filter(
+        field_name="state__country__continent",
+    )
+
+    country = filters.Filter(
+        field_name="state__country",
+    )
+
     class Meta:
         model = models.City
-        fields = ["state"]
+        fields = "__all__"
